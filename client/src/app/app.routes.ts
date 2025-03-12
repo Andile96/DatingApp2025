74832931ac73ed401d25e7a1e27170b49aaef58f
Andile96 <102928255+Andile96@users.noreply.github.com>
2025-03-12 16:04:06 +0200

    Section 7 - Assign 2 completed

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { authGuard } from './_guards/auth.guard';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 export const routes: Routes = [
     {path:'',component: HomeComponent},
@@ -25,6 +26,7 @@ export const routes: Routes = [
         ]
     },
     {path:'errors',component: TestErrorsComponent},
+    {path:'contact-us',component: ContactUsComponent},
     {path:'not-found',component: NotFoundComponent},
     {path:'server-error',component: ServerErrorComponent},
     {path:'**',component: HomeComponent,pathMatch:'full'},
