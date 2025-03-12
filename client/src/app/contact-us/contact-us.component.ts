53cb5e13a657bad3d8cab72be43ba29b2dc9ddac
Andile96 <102928255+Andile96@users.noreply.github.com>
2025-03-12 15:40:24 +0200

    Assignment 2ContactPage

---
+++ client/src/app/contact-us/contact-us.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Developer } from '../_models/developer';
+
+@Component({
+  selector: 'app-contact-us',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-us.component.html',
+  styleUrl: './contact-us.component.css'
+})
+export class ContactUsComponent {
+
+  developer : Developer = {
+    developerName : 'Innocent Andile Mnisi',
+    developerEmail : '2019728480@ufs4life.ac.za',
+    developerBio : 'CSIP6833 student passionate about Angular'
+  };
+
+}
