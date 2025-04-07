c46521ceceb0f7250ca865f59ef5afc23c61114c
Andile96 <102928255+Andile96@users.noreply.github.com>
2025-04-07 17:08:58 +0200

    Section12Client&ServerSiteValidation

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,15 +1,15 @@
 import { Component,inject,input, OnInit, output, Output } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
-import { ToastrService } from 'ngx-toastr';
-import { JsonPipe, NgIf } from '@angular/common';
+import {  NgIf } from '@angular/common';
 import { TextInputComponent } from "../_forms/text-input/text-input.component";
 import { DatePickerComponent } from "../_forms/date-picker/date-picker.component";
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-register',
     standalone: true,
-    imports: [ReactiveFormsModule, JsonPipe, TextInputComponent, NgIf, DatePickerComponent],
+    imports: [ReactiveFormsModule, TextInputComponent, NgIf, DatePickerComponent],
     templateUrl: './register.component.html',
     styleUrl: './register.component.css'
 })
@@ -20,11 +20,11 @@ throw new Error('Method not implemented.');
   
   private accountService = inject(AccountService);
   private fb =inject(FormBuilder);
-  private toastr = inject(ToastrService);
+  private router = inject(Router);
   cancelRegister = output<boolean>();
-  model: any = {};
   registerForm: FormGroup = new FormGroup({});
   maxDate: Date = new Date();
+  validationErrors: string[] | undefined;
 
   ngOnInit(): void {
     this.initializeForm();
@@ -54,16 +54,23 @@ throw new Error('Method not implemented.');
     };
   }
   register() {
-    this.accountService.Register(this.model).subscribe({
-      next: response => {
-        console.log(response);
-        this.cancel();
-      },
-      error:error=> this.toastr.error(error.error)
+    const dob = this.getDateOnly(this.registerForm.get('dateOfBirth')?.value);
+    this.registerForm.patchValue({ dateOfBirth: dob });
+    this.accountService.Register(this.registerForm.value).subscribe({
+      next: _ =>
+
+         this.router.navigateByUrl('/members'),
+        error:error=> this.validationErrors = error,
+      
+     
     });
   }
 
   cancel() {
     this.cancelRegister.emit(false);
+  }
+  private getDateOnly(dob: string | undefined) {
+    if (!dob) return;
+    return new Date(dob).toISOString().slice(0,10);
   }
 }
