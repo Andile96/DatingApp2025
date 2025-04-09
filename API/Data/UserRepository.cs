4aa34cab7686a266f5cc7027e54615a6462f5169
Andile96 <102928255+Andile96@users.noreply.github.com>
2025-04-09 13:13:53 +0200

    Section13OderLastActiveDateFormatting

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -32,6 +32,11 @@ namespace API.Data
             var maxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MinAge));
 
             query = query.Where(x => x.DateOfBirth >= minDob && x.DateOfBirth <= maxDob);
+            query = userParams.OrderBy switch
+            {
+                "created" => query.OrderByDescending(u => u.Created),
+                _ => query.OrderByDescending(u => u.LastActive)
+            };
            
 
             return await PagedList<MemberDto>.CreateAsync(query.ProjectTo<MemberDto>(mapper.ConfigurationProvider), 
