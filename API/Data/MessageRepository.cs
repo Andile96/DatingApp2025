7a27aac41995c991a516e4e3167aa5542f33b7d0
Andile96 <102928255+Andile96@users.noreply.github.com>
2025-04-16 23:10:52 +0200

    Section15Api&ClientDeleteMessage

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -34,10 +34,13 @@ namespace API.Data
 
             query = messageParams.Container switch
             {
-                "Inbox" => query.Where(x => x.Recipient.UserName == messageParams.Username),
-                "Outbox" => query.Where(x => x.Sender.UserName == messageParams.Username),
+                "Inbox" => query.Where(x => x.Recipient.UserName == messageParams.Username 
+                && x.RecipientDeleted == false),
+                "Outbox" => query.Where(x => x.Sender.UserName == messageParams.Username
+                && x.SenderDeleted == false),
                 _ => query.Where(x => x.Recipient.UserName ==
-                        messageParams.Username && x.DateRead == null)
+                        messageParams.Username && x.DateRead == null 
+                        && x.RecipientDeleted == false)
             };
 
             var messages = query.ProjectTo<MessageDto>(mapper.ConfigurationProvider);
@@ -52,10 +55,12 @@ namespace API.Data
                 .Include(x => x.Sender).ThenInclude(x => x.Photos)
                 .Include(x => x.Recipient).ThenInclude(x => x.Photos)
                 .Where(x => 
-                    x.RecipientUsername == currentUsername && 
-                    x.SenderUsername == recipientUsername || 
-                    x.SenderUsername == currentUsername && 
-                    x.RecipientUsername == recipientUsername  
+                    x.RecipientUsername  == currentUsername 
+                    && x.RecipientDeleted ==false
+                    && x.SenderUsername == recipientUsername || 
+                    x.SenderUsername == currentUsername 
+                    && x.SenderDeleted == false
+                    && x.RecipientUsername == recipientUsername  
                     
                 )
                 .OrderBy(x => x.MessageSent)
--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -28,6 +28,20 @@ export class MessagesComponent implements OnInit {
   loadMessages() {
     this.messageService.getMessages(this.pageNumber, this.pageSize, this.container);
   }
+  deleteMessage(id: number) {
+    this.messageService.deleteMessage(id).subscribe({
+      next: _ => {
+        this.messageService.paginatedResult.update(prev => {
+          if (prev && prev.items) {
+            prev.items.splice(prev.items.findIndex(m => m.id === id), 1);
+            return prev;
+          }
+          return prev;
+        })
+      }
+    });
+  }
+
 
   pageChanged(event: any) {
     if (this.pageNumber !== event.page) {
